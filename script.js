b408e07da38907d49f05c4f01614eb48662c27a9
nakamura0736 <tel07360102@gmail.com>
2020-11-27 01:31:29 +0900

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+const routes = [
+  { path: '/', component: httpVueLoader('page1.vue') },
+  { path: '/page1', component: httpVueLoader('page1.vue') },
+  { path: '/page2', component: httpVueLoader('page2.vue') }
+]
+
+const router = new VueRouter({
+  routes
+})
+
+const app = new Vue({
+  router
+}).$mount('#app')
